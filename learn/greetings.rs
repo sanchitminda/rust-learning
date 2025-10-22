f526cb6340aff9243c0b49876d2f6d30d3b2a829
keval minda <kevalminda29@gmail.com>
2025-10-22 18:49:33 +0530

    my hello world

---
+++ learn/greetings.rs
@@ -0,0 +1,9 @@
+pub fn welcome_message(name: &str) {
+    println!("---");
+    println!("Welcome, {}! This message came from the greetings.rs file.", name);
+    println!("---");
+}
+fn  main (){
+    welcome_message("keval");
+
+}
